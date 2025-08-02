460acaeee3301587180196b73eca828f4f0a0486
AkhilaM-21 <merugu.akhila21@gmail.com>
2025-08-02 13:55:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,14 +23,13 @@ app.listen(port,()=>{
 
 require('dotenv').config();
 
-const dbUrl = new URL(process.env.DATABASE_URL);
 
 const db = mysql.createConnection({
-  host: dbUrl.hostname,
-  user: dbUrl.username,
-  password: dbUrl.password,
-  database: dbUrl.pathname.replace('/', ''),
-  port: dbUrl.port
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   allowPublicKeyRetrieval: true
 });
 db.connect((err)=>
