1b0596e4982b740af230d39b0d2cc4fdcfbdefdd
AkhilaM-21 <merugu.akhila21@gmail.com>
2025-08-02 14:15:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,16 +21,19 @@ app.listen(port,()=>{
 
 });
 
+require('dotenv').config();
+
+const mysql = require('mysql2');
+
 const db = mysql.createConnection({
-  host: 'trolley.proxy.rlwy.net',
-  port: 14998,
-  user: 'root',
-  password: 'qEYFmhdmHxFYTKPjPGgefEqDtJSgFrTT',
-  database: 'railway',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   allowPublicKeyRetrieval: true,
   ssl: false
 });
-
 
 db.connect((err)=>
 {
