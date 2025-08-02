d97e6ca9177c6dab256f8e79533319ec862c66ec
AkhilaM-21 <merugu.akhila21@gmail.com>
2025-08-02 11:21:18 +0530

    Update client.js

--- public/js/client.js
+++ public/js/client.js
@@ -11,7 +11,7 @@ form.addEventListener('submit', (e) => {
     const investment = document.getElementById("investment").value.trim();
 
     if (validate()) {
-        fetch("https://cognifyproject.onrender.com", {
+        fetch("https://cognifyproject.onrender.com/register", {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
